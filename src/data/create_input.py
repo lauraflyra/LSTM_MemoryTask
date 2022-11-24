9e80715c17b43cbd24ce967c1110574139e15844
Laura <lauraflyra@gmail.com>
2022-11-24 12:20:16 +0100

    input class, partial testing

--- src/data/create_input.py
+++ src/data/create_input.py
@@ -1,14 +1,11 @@
 import numpy as np
 
 TIMES = np.array([9, 10, 11, 12, 13, 14, 15, 16], dtype=int)  # daily time slots of the Prof.
-
 PEOPLE = ["Laura L.", "Joram", "Dennis",
           "Felix", "Rob", "Friedrich", "Loreen", "Mark", "Laura N."]
 
 TOTAL_TIME_STEPS = 21
-
 INPUT_SIZE = 2  # one neuron always receives an input with 2 dimensions: time slots and people
-
 
 def lookup_table(people):
     """
@@ -20,11 +17,8 @@ def lookup_table(people):
     table = dict()
     for person in people:
         if person not in table:
-            table[person] = len(table)
+            table[person] = len(table) + 1  # It doesn't make sense to have entry 0
     return table
-
-
-DICT_PEOPLE = lookup_table(PEOPLE)
 
 
 def people2ix(schedule, lookup):
@@ -38,24 +32,36 @@ def people2ix(schedule, lookup):
 
 
 class Input:
-    def __init__(self, batch_size=200):
+    def __init__(self, batch_size=200, people = PEOPLE):
         self.batch_size = batch_size
         self.input = np.zeros((TOTAL_TIME_STEPS, self.batch_size, INPUT_SIZE))
-
-        self.init_input_times()
-        self.combine_people_and_times()
+        self.people = people
+        self.dict_people = lookup_table(self.people)
+        self.schedules = []
 
     def init_input_times(self):
-        self.input[0:len(TIMES), :, 0] = TIMES
+        self.input[0:len(TIMES), :, 0] = np.stack((TIMES,) * self.batch_size, axis=-1)
 
     def combine_people_and_times(self):
-        p2ix = people2ix(PEOPLE, DICT_PEOPLE)
-        schedules = np.random.choice(p2ix, size=(len(TIMES), self.batch_size), replace=True)
-        self.input[0:len(TIMES), :, 1] = schedules
+        p2ix = people2ix(self.people, self.dict_people)
+        self.schedules = np.random.choice(p2ix, size=(len(TIMES), self.batch_size), replace=True)
+        self.input[0:len(TIMES), :, 1] = self.schedules
 
-    # def select_query(self):
-    #     queries = np.random.choice(TIMES, size=self.batch_size, replace=True)
-    #     possible_query_times = np.arange(len(TIMES)+1, TOTAL_TIME_STEPS, 1)
-    #     queries_times = np.random.choice(possible_query_times, size=self.batch_size, replace=True)
 
+    def select_query(self):
+        queries = np.random.choice(TIMES, size=self.batch_size, replace=True)
+        possible_query_times = np.arange(len(TIMES)+1, TOTAL_TIME_STEPS, 1)
+        queries_times = np.random.choice(possible_query_times, size=self.batch_size, replace=True)
+        for i in range(self.batch_size):
+            self.input[queries_times[i], i, 0] = queries[i]
+
+    def finalize_input(self):
+        self.init_input_times()
+        self.combine_people_and_times()
+        self.select_query()
+
+
+if __name__ == "__main__":
+    xy_input = Input()
+    xy_input.finalize_input()
 
--- tests/test_preTrain.py
+++ tests/test_preTrain.py
@@ -10,6 +10,9 @@ There's some tests that we can run without needing trained parameters. These tes
 
 The main goal here is to identify some errors early so we can avoid a wasted training job."""
 
+import unittest
+import numpy as np
+from src.data.create_input import Input
 
 def test_lookup_table():
     from src.data.create_input import lookup_table
@@ -22,8 +25,23 @@ def test_people2ix():
     from src.data.create_input import people2ix
     from test_helpers import mock_schedule, mock_int_schedule, mock_table
     int_schedule = people2ix(mock_schedule, mock_table)
-    assert mock_int_schedule == int_schedule
+    assert (mock_int_schedule == int_schedule).all()
 
 
+class TestInput(unittest.TestCase):
 
-#class TestDataset(unittest.TestCase):
+    def test_init_input_times(self):
+        from src.data.create_input import TIMES, TOTAL_TIME_STEPS
+        xy = Input(batch_size=1)
+        xy.init_input_times()
+        self.assertEqual(xy.input[:,0,0].tolist(),np.pad(TIMES, (0,TOTAL_TIME_STEPS-len(TIMES)), 'constant', constant_values=0).tolist())
+
+    def test_combine_people_and_times(self):
+        ...
+
+    def test_select_query(self):
+        ...
+
+
+if __name__ == '__main__':
+    unittest.main()
