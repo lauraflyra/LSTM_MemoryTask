787491c082b4556336a594499aa45cf41650e966
Laura <lauraflyra@gmail.com>
2022-12-07 15:00:02 +0100

    plot input and output function

---
+++ src/visualization/plot_input_output.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from src.data.create_input import create_input, create_output, create_go_signal
+
+def plot_input(x, y):
+    tot_time_steps, batch_size, input_size = x.shape
+    which_from_batch = np.random.randint(batch_size, size = 1)
+    spacing = np.linspace(0,10, input_size)
+    x_plot = x[:,which_from_batch,:].reshape(tot_time_steps, input_size)+spacing
+    y_plot = y[:, which_from_batch, :].reshape(tot_time_steps, input_size) + spacing
+    time_array = np.arange(0, tot_time_steps, 1)
+    fig, axs = plt.subplots(nrows=1,ncols=2)
+    axs[0].plot(time_array, x_plot[:,0:4], color = 'blue')
+    axs[0].plot(time_array, x_plot[:, 4:8], color='red')
+    axs[0].plot(time_array, x_plot[:, 8:], color='orange')
+    axs[0].set_title("input")
+    axs[1].plot(time_array, y_plot[:,0:4], color = 'blue')
+    axs[1].plot(time_array, y_plot[:, 4:8], color='red')
+    axs[1].plot(time_array, y_plot[:, 8:], color='orange')
+    axs[1].set_title("output")
+    plt.show()
+
+if __name__ == "__main__":
+    x = create_input()
+    x, go_signal_idx, go_signal_moments = create_go_signal(x)
+    output = create_output(x, go_signal_idx, go_signal_moments)
+    plot_input(x, output)
+
+
