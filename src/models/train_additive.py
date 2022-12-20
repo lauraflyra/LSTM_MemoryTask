9fdcf080efcb7aa1c152d6956b440d5bdf495a4c
Laura <lauraflyra@gmail.com>
2022-12-20 19:15:47 +0100

    add plotting functions for additive, train additive

--- src/models/train_additive.py
+++ src/models/train_additive.py
@@ -1,13 +1,43 @@
-from src.data.create_input_output_additive import *
-from src.models.network_additive import NetworkAddtive
+from src.data.create_input_output_additive import create_input_go_add, create_output_add
+from src.models.network_additive import NetworkAdditive
+from src.visualization.plot_input_output import plot_result_training_additive
 import torch
 import torch.nn as nn
+import scipy.io
+import os
 
 
-def train(dataset, n_epochs = 1000, saveLossEvery = 100):
+DATA_PATH = "/home/lauraflyra/Documents/SHK_SprekelerLab/LSTM_computations/LSTM_MemoryTask/src/data/"
+
+
+def train(dataset, model, n_epochs, saveParams=True, saveModel=True, outputTraining=True,
+          saveParamsName=os.path.join(DATA_PATH, "params.mat"),
+          saveModelName=os.path.join(DATA_PATH, "model.pt"), saveLossEvery=100, saveParamsEvery=100):
+    """
+    :param dataset:
+    :param model:
+    :param n_epochs:
+    :param saveParams:
+    :param saveModel:
+    :param outputTraining:
+    :param saveParamsName:
+    :param saveModelName:
+    :param saveLossEvery:
+    :param saveParamsEvery:
+    :return:
+    """
+
     loss_fn = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-    x, output = dataset
+    optimizer = torch.optim.Adam(model.parameters(), lr = 0.01)
+
+    x, output = dataset  # TODO: add more to the dataset if needed, like cue times or go signal info
+
+    train_error = []
+
+    store_non_linearity = {"x": [], "output": [], "training_error": []}
+    if saveParams:
+        store_non_linearity["x"].append(x.detach().numpy())
+        store_non_linearity["output"].append(output.detach().numpy())
 
     for epoch in range(n_epochs):
         out_pred, model_variables = model(x)
@@ -17,12 +47,31 @@ def train(dataset, n_epochs = 1000, saveLossEvery = 100):
         loss.backward()
         optimizer.step()
 
-        if epoch % saveLossEvery == 0 :
+        if epoch % saveLossEvery == 0 and outputTraining:
             print("training:", epoch, loss.item())
+            store_non_linearity["training_error"].append(loss.item())
+            train_error.append(loss.item())
 
-    return x, out_pred
+        if epoch % saveParamsEvery == 0 and saveParams:
+            if epoch == 0:
+                for count, neuron in enumerate(model_variables.keys()):
+                    store_non_linearity[neuron + "-g"] = []
+            for count, neuron in enumerate(model_variables.keys()):
+                store_non_linearity[neuron + "-g"].append(torch.stack(model_variables[neuron]['g'][1:]).detach().numpy())
+
+    if saveParams:
+        scipy.io.savemat(saveParamsName, store_non_linearity)
+    if saveModel:
+        torch.save(model, saveModelName)
+
+    return x, output, out_pred, train_error
+
 
 if __name__ == "__main__":
-    model = NetworkAddtive()
+    x, go_signal_time_slots, go_signal_moments = create_input_go_add(batch_size=100)
+    y = create_output_add(x, go_signal_time_slots, go_signal_moments)
+    model = NetworkAdditive()
     dataset = (torch.from_numpy(x).float(), torch.from_numpy(y).float())
-    train(dataset)
+    x, output, out_pred, train_error = train(dataset, model, n_epochs=500)
+    which_from_batch = plot_result_training_additive(x, output, out_pred.detach().numpy(), train_error, n_epochs=500,
+                                                      plot_every=100)
--- src/visualization/plot_input_output.py
+++ src/visualization/plot_input_output.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from src.data.common_vars import PEOPLE, TIME_SLOTS
+from src.data.create_input_output_additive import create_input_go_add, create_output_add
 from src.data.create_input_output_factorial import create_input, create_output, create_go_signal
 import matplotlib.patches as mpatches
 
@@ -62,10 +64,70 @@ def plot_result_training_factorial(x, output, out_pred, train_error, n_epochs, p
     return which_from_batch
 
 
+def plot_input_additive(x):
+    tot_time_steps, batch_size, input_size = x.shape
+    which_from_batch = np.random.randint(batch_size, size=1)
+    spacing = np.arange(0, input_size, 1)
+    x_plot = x[:, which_from_batch, :].reshape(tot_time_steps, input_size) - spacing
+    time_array = np.arange(0, tot_time_steps, 1)
+    plt.plot(time_array, x_plot[:,:len(PEOPLE)])
+    plt.plot(time_array, x_plot[:, len(PEOPLE):], color='black', label = "time slot")
+    handles, labels = plt.gca().get_legend_handles_labels()
+    by_label = dict(zip(labels, handles))
+    plt.legend(by_label.values(), by_label.keys())
+    plt.title("input")
+    plt.show()
+    return
+
+def plot_result_training_additive(x, output, out_pred, train_error, n_epochs, plot_every):
+    tot_time_steps, batch_size, input_size = x.shape
+    which_from_batch = np.random.randint(batch_size, size=1)
+    spacing_y = np.arange(0, len(PEOPLE), 1)
+    spacing_x = np.arange(0, input_size, 1)
+    x_plot = x[:, which_from_batch, :].reshape(tot_time_steps, input_size) + spacing_x
+    output_plot = output[:, which_from_batch, :].reshape(tot_time_steps, len(PEOPLE)) + spacing_y
+    out_pred_plot = out_pred[:, which_from_batch, :].reshape(tot_time_steps, len(PEOPLE)) + spacing_y
+    time_array = np.arange(0, tot_time_steps, 1)
+
+
+    plt.plot(time_array, x_plot[:,:len(PEOPLE)])
+    plt.plot(time_array, x_plot[:, len(PEOPLE):], color='black', label = "time slot")
+    handles, labels = plt.gca().get_legend_handles_labels()
+    by_label = dict(zip(labels, handles))
+    plt.legend(by_label.values(), by_label.keys())
+    plt.title("input")
+    plt.show()
+
+    plt.plot(time_array, output_plot, color = 'blue', linewidth = 3)
+    plt.plot(time_array, out_pred_plot, color ='black', alpha=0.6, label = 'predicted')
+    plt.title("output")
+
+    plt.xlabel('time points')
+    plt.xlabel('time points')
+    plt.ylabel('neuron number')
+
+    # black_patch = mpatches.Patch(color='black', label='predicted')
+    handles, labels = plt.gca().get_legend_handles_labels()
+    by_label = dict(zip(labels, handles))
+    plt.legend(by_label.values(), by_label.keys(), loc='lower right')
+    plt.show()
+
+    plt.plot(np.linspace(plot_every, n_epochs, len(train_error[1:])),train_error[1:])
+    plt.xlabel('epochs')
+    plt.title("Training error")
+    plt.show()
+    return which_from_batch
+
+
+
+
 if __name__ == "__main__":
     x = create_input()
     x, go_signal_idx, go_signal_moments = create_go_signal(x)
     output = create_output(x, go_signal_idx, go_signal_moments)
     plot_input_factorial(x, output)
+    x, go_signal_time_slots, go_signal_moments = create_input_go_add()
+    output = create_output_add(x, go_signal_time_slots, go_signal_moments)
+    plot_input_additive(x)
 
 
