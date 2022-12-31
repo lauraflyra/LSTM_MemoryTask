5fd0a4597a98dd75d01591d352943c903914c204
Laura <lauraflyra@gmail.com>
2022-12-31 17:29:27 +0100

    add function for plotting the result a multiple random input/output pairs

--- src/visualization/plot_input_output.py
+++ src/visualization/plot_input_output.py
@@ -4,6 +4,9 @@ from src.data.common_vars import PEOPLE, TIME_SLOTS
 from src.data.create_input_output_additive import create_input_go_add, create_output_add
 from src.data.create_input_output_factorial import create_input, create_output, create_go_signal
 import matplotlib.patches as mpatches
+import torch
+import os
+import scipy.io as io
 
 def plot_input_factorial(x, y):
     tot_time_steps, batch_size, input_size = x.shape
@@ -119,9 +122,65 @@ def plot_result_training_additive(x, output, out_pred, train_error, n_epochs, pl
     return which_from_batch
 
 
+def plot_result_multiple_training_additive(params_file, model_file):
+    SAVE_DATA_PATH = "/home/lauraflyra/Documents/SHK_SprekelerLab/LSTM_computations/LSTM_MemoryTask/src/data/data_all_neurons_separate_linear_additive_random_batch"
+
+    model = torch.load(model_file)
+    data = io.loadmat(params_file, struct_as_record=False, squeeze_me=True)
+    # take random samples from batch
+    time_steps, total_batch, input_dim = data["x"].shape
+    random_batch = np.random.randint(0, total_batch, size=10)
+    x = data['x'][:,random_batch,:]
+    y = data['output'][:,random_batch,:]
+    out_pred, model_variables = model(torch.from_numpy(x).float())
+
+    spacing_y = np.arange(0, len(PEOPLE), 1)
+    spacing_x = np.arange(0, input_dim, 1)
+
+    time_array = np.arange(0, time_steps, 1)
+
+    out_pred = out_pred.detach().numpy()
+    for i in range(10):
+        x_plot = x[:, i, :] + spacing_x
+        output_plot = y[:, i, :] + spacing_y
+        out_pred_plot = out_pred[:, i, :] + spacing_y
+
+        plt.plot(time_array, x_plot[:,:len(PEOPLE)])
+        plt.plot(time_array, x_plot[:, len(PEOPLE):], color='black', label = "time slot")
+        handles, labels = plt.gca().get_legend_handles_labels()
+        by_label = dict(zip(labels, handles))
+        plt.legend(by_label.values(), by_label.keys())
+        plt.title("input training random batch n {}".format(i))
+        name = "additive_input_random_batch_"+str(i)+".png"
+        plt.savefig(os.path.join(SAVE_DATA_PATH, name))
+        plt.show()
+
+        plt.plot(time_array, output_plot, color = 'blue', linewidth = 3)
+        plt.plot(time_array, out_pred_plot, color ='black', alpha=0.6, label = 'predicted')
+        plt.title("output training random batch n {}".format(i))
+
+        plt.xlabel('time points')
+        plt.xlabel('time points')
+        plt.ylabel('neuron number')
+
+        # black_patch = mpatches.Patch(color='black', label='predicted')
+        handles, labels = plt.gca().get_legend_handles_labels()
+        by_label = dict(zip(labels, handles))
+        plt.legend(by_label.values(), by_label.keys(), loc='lower right')
+        name = "additive_output_random_batch_"+str(i)+".png"
+        plt.savefig(os.path.join(SAVE_DATA_PATH, name))
+        plt.show()
+
+    return
 
 
 if __name__ == "__main__":
+    DATA_PATH = "/home/lauraflyra/Documents/SHK_SprekelerLab/LSTM_computations/LSTM_MemoryTask/src/data/"
+
+    plot_result_multiple_training_additive(os.path.join(DATA_PATH, "params_additive.mat"),
+                            os.path.join(DATA_PATH, "model_additive_all_neurons_separate_linear.pt"))
+
+
     x = create_input()
     x, go_signal_idx, go_signal_moments = create_go_signal(x)
     output = create_output(x, go_signal_idx, go_signal_moments)
