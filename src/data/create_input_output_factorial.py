17234b384a5d5dd52ad0dc02a47ccb2a6e8bec0d
Laura <lauraflyra@gmail.com>
2023-01-12 19:05:23 +0100

    rename variables to match the ones in additive case, create gen_input_output function to match the one neuron case

--- src/data/create_input_output_factorial.py
+++ src/data/create_input_output_factorial.py
@@ -1,5 +1,6 @@
 import numpy as np
 from src.data.common_vars import PEOPLE, TIME_SLOTS
+
 TOTAL_TIME_STEPS = 20
 INPUT_SIZE = len(PEOPLE) * len(TIME_SLOTS)
 BATCH_SIZE = 700
@@ -25,33 +26,35 @@ def create_go_signal(x):
     batch_size = x.shape[1]
     # insert go signal
     # first we decide when the go signal is gonna happen
-    possible_go_signal_moments = np.arange(CUE_TIME + 2, TOTAL_TIME_STEPS-1, 1)
+    possible_go_signal_moments = np.arange(CUE_TIME + 2, TOTAL_TIME_STEPS - 1, 1)
     go_signal_moments = np.random.choice(possible_go_signal_moments, size=batch_size, replace=True)
     # then we decide which time slot we want to recover
-    go_signal_idx = np.random.choice(range(len(TIME_SLOTS)), size=batch_size, replace=True)
+    go_signal_time_slots = np.random.choice(range(len(TIME_SLOTS)), size=batch_size, replace=True)
     for i in range(batch_size):
-        x[go_signal_moments[i], i, go_signal_idx[i] * 4:go_signal_idx[i] * 4 + 4] = 0.5
+        x[go_signal_moments[i], i, go_signal_time_slots[i] * 4:go_signal_time_slots[i] * 4 + 4] = 0.5
 
     for i in range(batch_size):
         assert np.sum(x[go_signal_moments[i], i, :]) == 4 * 0.5
-    return x, go_signal_idx, go_signal_moments
+    return x, go_signal_time_slots, go_signal_moments
 
 
-def create_output(x, go_signal_idx, go_signal_moments):
+def create_output(x, go_signal_time_slots, go_signal_moments):
     # create output based on queries
     batch_size = x.shape[1]
     output = np.zeros((TOTAL_TIME_STEPS, batch_size, INPUT_SIZE))  # Output is gonna be in one-hot encoding of people
     # insert response to cue
     output[CUE_TIME, :, :] = x[CUE_TIME, :, :]
     # insert response to go signal
-    for batch_item, i in enumerate(go_signal_idx):
+    for batch_item, i in enumerate(go_signal_time_slots):
         output[go_signal_moments[batch_item], batch_item, i * 4:i * 4 + 4] = x[CUE_TIME, batch_item, i * 4:i * 4 + 4]
     return output
 
 
-if __name__ == "__main__":
-    x = create_input()
-    x, go_signal_idx, go_signal_moments = create_go_signal(x)
-    output = create_output(x, go_signal_idx, go_signal_moments)
+def gen_input_output(batch_size):
+    x = create_input(batch_size=batch_size)
+    x, go_signal_time_slots, go_signal_moments = create_go_signal(x)
+    output = create_output(x, go_signal_time_slots, go_signal_moments)
+    return x, output, go_signal_time_slots, go_signal_moments
+
 
 
