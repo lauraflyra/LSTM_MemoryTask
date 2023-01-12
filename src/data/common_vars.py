dac00ac6c6a6041b190e9d884190960fb127e2e4
Laura <lauraflyra@gmail.com>
2023-01-12 19:04:27 +0100

    add path to results folder to common variables script

--- src/data/common_vars.py
+++ src/data/common_vars.py
@@ -2,3 +2,4 @@ import numpy as np
 
 PEOPLE = ["Laura", "Joram", "Denis", "Felix"]
 TIME_SLOTS = np.array([9,10,11], dtype=int)
+DATA_PATH = "/home/lauraflyra/Documents/SHK_SprekelerLab/LSTM_computations/LSTM_MemoryTask/src/data/results"
