31198f86a99b97a727d3ccb92edcd9a7e6ddeb3f
Laura <lauraflyra@gmail.com>
2023-01-13 16:11:00 +0100

    add new plotting functions to after train

--- src/models/train_additive.py
+++ src/models/train_additive.py
@@ -18,21 +18,15 @@ dict_results_additive = train(dataset,
                               save_model_name=os.path.join(PATH_RESULTS_ADDITIVE, "model_additive.pt"),
                               checkpoints_file_name="model_checkpoints_factorial")
 
-#
-# if __name__ == "__main__":
-#     x, go_signal_time_slots, go_signal_moments = create_input_go_add(batch_size=500)
-#     y = create_output_add(x, go_signal_time_slots, go_signal_moments)
-#     model = NetworkAdditive()
-#     dataset = (torch.from_numpy(x).float(), torch.from_numpy(y).float())
-#     x, output, out_pred, train_error = train(dataset, model, n_epochs=1010,
-#                         saveParamsName=os.path.join(DATA_PATH, "params_model_additive_more_dimensions_neurons.mat"),
-#                         saveModelName=os.path.join(DATA_PATH, "model_additive_more_dimensions_neurons.pt"))
-#     which_from_batch = plot_result_training_additive(x, output, out_pred.detach().numpy(), train_error, n_epochs=1010,
-#                                                       plot_every=100)
-#
-#     plot_AF_separate_neurons_add(params_file=os.path.join(DATA_PATH, "params_model_additive_more_dimensions_neurons.mat"), go_signal_time_slots=go_signal_time_slots,
-#             go_signal_moments=go_signal_moments, which_from_batch=which_from_batch)
-#
-#     # TODO: see if there are differences for longer, shorter cue times
-#     # TODO: sample more from the results
-#     # TODO: how to save model over the course of learning? How is learning?
+
+from src.visualization.plot_input_output import plot_result_training_additive
+
+which_from_batch = plot_result_training_additive(params_file=os.path.join(PATH_RESULTS_ADDITIVE, "params_additive.mat"))
+
+from src.visualization.plot_activation_functions import plot_af_additive_neurons
+
+plot_af_additive_neurons(params_file=os.path.join(PATH_RESULTS_ADDITIVE, "params_additive.mat"),
+                         which_from_batch=which_from_batch)
+
+# For plotting the results of the training for more than one sample from the batch,
+# use function plot_result_multiple_training_additive!
