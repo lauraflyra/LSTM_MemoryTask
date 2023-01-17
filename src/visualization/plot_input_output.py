a7a506032be388ca7b91b2eed3032c455c06ec6c
Laura <lauraflyra@gmail.com>
2023-01-17 12:50:06 +0100

    plotting functions for results of VGeadah AF training

--- src/visualization/plot_input_output.py
+++ src/visualization/plot_input_output.py
@@ -7,6 +7,75 @@ import scipy.io as io
 import matplotlib.colors as mcolors
 
 
+
+def plot_result_training_one_neuron_Geadah(params_file):
+
+    data = io.loadmat(params_file, struct_as_record=False, squeeze_me=True)
+    x = data['x']
+    output = data['y']
+    out_pred = data['y_pred']
+    train_error = data['training_error']
+    n_epochs = data['n_epochs']
+    plot_every = data['save_loss_every']
+    cue_amps = data['cue_amp']
+
+    which_plot_no_bump = np.where(np.abs(cue_amps) < 0.5)[0][0]
+    which_plot_wt_bump = np.where(np.abs(cue_amps) >= 0.5)[0][0]
+
+    fig, axs = plt.subplots(2, 1, figsize=(12, 12))
+    axs[0].plot(x[:, which_plot_no_bump], label="input", linewidth = 5, alpha = 0.5)
+    axs[0].set_title("No cue", fontsize = 17)
+    axs[0].set_xlabel("Time", fontsize = 17)
+    axs[0].set_ylabel("Neural response/ Input strength", fontsize=17)
+    axs[0].legend()
+    #
+    axs[0].plot(output[:, which_plot_no_bump], label="target output", linewidth = 5, alpha = 0.7)
+    #
+
+    axs[0].plot(out_pred[-1, :, which_plot_no_bump], label="predicted output", linewidth = 5, alpha = 0.5)
+    axs[0].legend()
+
+    axs[1].plot(x[:, which_plot_wt_bump], label="input", linewidth = 5, alpha = 0.5)
+    axs[1].set_title("Cue and go", fontsize = 17)
+    axs[1].set_xlabel("Time", fontsize = 17)
+    axs[1].set_ylabel("Neural response/ Input strength", fontsize=17)
+    axs[1].legend()
+    #
+    axs[1].plot(output[:, which_plot_wt_bump], label="target output", linewidth = 5, alpha = 0.7)
+    #
+
+    axs[1].plot(out_pred[-1,:, which_plot_wt_bump], label="predicted output", linewidth = 5, alpha = 0.5)
+    axs[1].legend()
+    axs[0].set_ylim(axs[1].get_ylim())
+    axs[0].spines[['right', 'top']].set_visible(False)
+    axs[1].spines[['right', 'top']].set_visible(False)
+    axs[0].spines[['right', 'top']].set_visible(False)
+    axs[1].spines[['right', 'top']].set_visible(False)
+
+    axs[0].spines[['bottom', 'left']].set_linewidth(4)
+    axs[1].spines[['bottom', 'left']].set_linewidth(4)
+
+    axs[0].tick_params(width=4)
+    axs[1].tick_params(width=4)
+
+    plt.show()
+
+    fig, ax = plt.subplots(1,1)
+    plt.plot(np.arange(plot_every, n_epochs, plot_every), train_error[1:], label="train error", linewidth = 4, alpha = 0.5)
+    plt.xlabel("Epochs", fontsize = 15)
+    plt.ylabel("MSE", fontsize = 15)
+    plt.title("Mean Squared Error during training", fontsize = 15)
+    ax.spines[['right', 'top']].set_visible(False)
+    for axis in ['bottom', 'left']:
+        ax.spines[axis].set_linewidth(4)
+    # increase tick width
+    ax.tick_params(width=4)
+    plt.show()
+
+    return
+
+
+
 def plot_input_factorial(x, y):
     tot_time_steps, batch_size, input_size = x.shape
     which_from_batch = np.random.randint(batch_size, size=1)
@@ -189,9 +258,9 @@ def plot_result_multiple_training_additive(params_file,
         handles, labels = plt.gca().get_legend_handles_labels()
         by_label = dict(zip(labels, handles))
         plt.legend(by_label.values(), by_label.keys())
-        plt.title("input training random batch n {}".format(i))
-        plt.xlabel("time points")
-        plt.ylabel("Neuron number")
+        plt.title("input training random batch n {}".format(i), fontsize = 15)
+        plt.xlabel("time points", fontsize = 15)
+        plt.ylabel("Neuron number", fontsize = 15)
         name = save_name_input + str(i) + ".png"
         plt.savefig(os.path.join(save_data_path, name))
         plt.show()
@@ -200,11 +269,10 @@ def plot_result_multiple_training_additive(params_file,
             plt.plot(time_array, output_plot[:, person_idx], color=colors[person_idx], linewidth=3)
 
         plt.plot(time_array, out_pred_plot, color='black', alpha=0.6, label='predicted')
-        plt.title("output training random batch n {}".format(i))
+        plt.title("output training random batch n {}".format(i), fontsize = 15)
 
-        plt.xlabel('time points')
-        plt.xlabel('time points')
-        plt.ylabel('neuron number')
+        plt.xlabel('time points', fontsize = 15)
+        plt.ylabel('neuron number', fontsize = 15)
 
         handles, labels = plt.gca().get_legend_handles_labels()
         by_label = dict(zip(labels, handles))
