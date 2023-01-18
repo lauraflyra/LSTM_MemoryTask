46d9ed3b53f906d79d427a281da5af4acf6d73e9
Laura <lauraflyra@gmail.com>
2023-01-18 12:04:59 +0100

    function for plotting activation function parameters in time for one neuron

--- src/visualization/plot_activation_functions.py
+++ src/visualization/plot_activation_functions.py
@@ -10,6 +10,122 @@ def geadah_activation_function(x, n, s):
     second_factor = s*(np.exp(n*x)/(1+np.exp(n*x)))
     return first_factor*second_factor
 
+def plot_params_af_in_time_one_neuron(params_file):
+    data = io.loadmat(params_file, struct_as_record=False, squeeze_me=True)
+    n_t_samples, time_points, batch_size = data['g'].shape
+
+    cue_amps = data['cue_amp']
+
+    which_plot_no_bump = np.random.choice(np.where(np.abs(cue_amps) < 0.5)[0])
+    which_plot_wt_bump = np.random.choice(np.where(np.abs(cue_amps) > 0.5)[0])
+
+    which_plots = [which_plot_no_bump, which_plot_wt_bump]
+
+    from src.data.create_input_output_one_neuron import CUE_START_TIME, CUE_END_TIME, CUE_DURATION, GO_DURATION
+
+    GO_START_TIME = data["go_start_time"]
+
+    colors = {
+        'greys': plt.cm.Greys(np.linspace(0.1, 1, int(time_points))),
+        'reds': plt.cm.Reds(np.linspace(0.5, 1, GO_DURATION)),
+        'greens': plt.cm.Greens(np.linspace(0.1, 1, CUE_DURATION)),
+        'purples': plt.cm.Purples(np.linspace(0.1, 1, CUE_DURATION + 1)), }
+
+    gs = data['g'][-1, :, which_plots]
+
+    z = np.linspace(-0.5, 1.5)
+
+    fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(15, 15))
+    axs[0, 0].plot(data['x'][:, which_plots[0]], linewidth = 4, alpha = 0.5)
+    axs[0, 0].set_xlabel('Time', fontsize = 15)
+    axs[0, 0].set_ylabel('Input strength', fontsize=15)
+    axs[0, 0].set_title('Input', fontsize = 15)
+    axs[1, 0].plot(data['x'][:, which_plots[1]], linewidth = 4, alpha = 0.5)
+    axs[1, 0].set_ylabel('Input strength', fontsize=15)
+    axs[1, 0].set_xlabel('Time', fontsize = 15)
+
+    GO_END_TIME = GO_START_TIME + GO_DURATION
+
+    OUTPUT_RESPONSE_START = GO_START_TIME
+    OUTPUT_RESPONSE_END = OUTPUT_RESPONSE_START + CUE_DURATION
+
+    GO_START_1 = GO_START_TIME[which_plots[0]]
+    GO_END_1 = GO_END_TIME[which_plots[0]]
+    GO_START_2 = GO_START_TIME[which_plots[1]]
+    GO_END_2 = GO_END_TIME[which_plots[1]]
+
+    OUT_START_1 = OUTPUT_RESPONSE_START[which_plots[0]]
+    OUT_END_1 = OUTPUT_RESPONSE_END[which_plots[0]]
+    OUT_START_2 = OUTPUT_RESPONSE_START[which_plots[1]]
+    OUT_END_2 = OUTPUT_RESPONSE_END[which_plots[1]]
+
+    for t in range(time_points):
+        if t > 0:
+            axs[0, 1].plot(z, activation_Function(z, gs[0, t], 1,1), color=colors['greys'][t])
+            axs[1, 1].plot(z, activation_Function(z, gs[1, t], 1,1), color=colors['greys'][t])
+
+        if CUE_START_TIME < t < CUE_END_TIME:
+            axs[0, 1].plot(z, activation_Function(z, gs[0, t], 1,1),
+                           color=colors['purples'][t - CUE_START_TIME], linewidth=4)
+            axs[1, 1].plot(z, activation_Function(z, gs[1, t], 1,1),
+                           color=colors['purples'][t - CUE_START_TIME], linewidth=4)
+            axs[0, 0].scatter(t, data['x'][t, which_plots[0]], color=colors['purples'][t - CUE_START_TIME], linewidth=4)
+            axs[1, 0].scatter(t, data['x'][t, which_plots[1]], color=colors['purples'][t - CUE_START_TIME], linewidth=4)
+
+        if OUT_START_1 <= t < OUT_END_1:
+            axs[0, 1].plot(z, activation_Function(z, gs[0, t], 1,1),
+                           color=colors['greens'][t - OUT_END_1], linewidth=3)
+            axs[0, 0].scatter(t, data['x'][t, which_plots[0]], color=colors['greens'][t - OUT_END_1], linewidth=10)
+
+        if OUT_START_2 <= t < OUT_END_2:
+            axs[1, 1].plot(z, activation_Function(z, gs[1, t], 1,1),
+                           color=colors['greens'][t - OUT_START_2], linewidth=3)
+            axs[1, 0].scatter(t, data['x'][t, which_plots[1]], color=colors['greens'][t - OUT_START_2], linewidth=10)
+
+        if GO_START_1 <= t < GO_END_1:
+            axs[0, 1].plot(z, activation_Function(z, gs[0, t], 1,1),
+                           color=colors['reds'][t - GO_START_1], linewidth=4)
+            axs[0, 0].scatter(t, data['x'][t, which_plots[0]], color=colors['reds'][t - GO_START_1], linewidth=10)
+
+        if GO_START_2 <= t < GO_END_2:
+            axs[1, 1].plot(z, activation_Function(z, gs[1, t], 1,1),
+                           color=colors['reds'][t - GO_START_2], linewidth=4)
+            axs[1, 0].scatter(t, data['x'][t, which_plots[1]], color=colors['reds'][t - GO_START_2], linewidth=10)
+
+
+    red_patch = mpatches.Patch(color=colors['reds'][-1], label='go signal times')
+    purple_patch = mpatches.Patch(color=colors['purples'][-1], label='cue times')
+    green_patch = mpatches.Patch(color=colors['greens'][-1], label='response times')
+
+    plt.legend(handles=[purple_patch, red_patch, green_patch], bbox_to_anchor=[1, 1])
+
+    axs[0, 1].set_title('Non-linearity', fontsize = 15)
+    axs[1, 1].set_title('Colors from light to dark represent time points'.format(which_plots[1]), fontsize = 15)
+    axs[0, 1].set_xlabel('z', fontsize = 15)
+    axs[1, 1].set_xlabel('z', fontsize = 15)
+    axs[0, 1].set_ylabel('gamma(z;n,s)', fontsize = 15)
+    axs[1, 1].set_ylabel('gamma(z;n,s)', fontsize = 15)
+
+    axs[0, 0].spines[['right', 'top']].set_visible(False)
+    axs[0, 1].spines[['right', 'top']].set_visible(False)
+    axs[1, 0].spines[['right', 'top']].set_visible(False)
+    axs[1, 1].spines[['right', 'top']].set_visible(False)
+
+    axs[0, 0].spines[['bottom', 'left']].set_linewidth(3)
+    axs[0, 1].spines[['bottom', 'left']].set_linewidth(3)
+    axs[1, 0].spines[['bottom', 'left']].set_linewidth(3)
+    axs[1, 1].spines[['bottom', 'left']].set_linewidth(3)
+
+    axs[0, 0].tick_params(width=3)
+    axs[0, 1].tick_params(width=3)
+    axs[1, 0].tick_params(width=3)
+    axs[1, 1].tick_params(width=3)
+
+    plt.show()
+    return
+
+
+
 def plot_params_af_in_time_one_neuron_learns_all_HAF(params_file):
     data = io.loadmat(params_file, struct_as_record=False, squeeze_me=True)
     n_t_samples, time_points, batch_size = data['g'].shape
@@ -48,8 +164,8 @@ def plot_params_af_in_time_one_neuron_learns_all_HAF(params_file):
 
     GO_END_TIME = GO_START_TIME + GO_DURATION
 
-    OUTPUT_RESPONSE_START = GO_END_TIME
-    OUTPUT_RESPONSE_END = GO_END_TIME + CUE_DURATION
+    OUTPUT_RESPONSE_START = GO_START_TIME
+    OUTPUT_RESPONSE_END = OUTPUT_RESPONSE_START + CUE_DURATION
 
     GO_START_1 = GO_START_TIME[which_plots[0]]
     GO_END_1 = GO_END_TIME[which_plots[0]]
@@ -165,7 +281,7 @@ def plot_params_af_in_time_one_neurons_Geadah(params_file):
     GO_END_TIME = GO_START_TIME + GO_DURATION
 
     OUTPUT_RESPONSE_START = GO_END_TIME
-    OUTPUT_RESPONSE_END = GO_END_TIME + CUE_DURATION
+    OUTPUT_RESPONSE_END = OUTPUT_RESPONSE_START + CUE_DURATION
 
     GO_START_1 = GO_START_TIME[which_plots[0]]
     GO_END_1 = GO_END_TIME[which_plots[0]]
